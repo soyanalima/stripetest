b69f29b2885a456f00f28bb9f9053bd335465a1d
soyanalima <ana.leidymar.lima@gmail.com>
2018-11-27 09:23:05 -0500

    Solving issues

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -4,7 +4,6 @@ from .views import (
 	MembershipSelectView, 
 	PaymentView, 
 	updateTransactionRecords,
-	profile_view,
 	cancelSubscription
 	)
 
@@ -14,6 +13,5 @@ urlpatterns = [
     path('', MembershipSelectView.as_view(), name='select'),
     path('payment/', PaymentView, name='payment'),
     path('update-transactions/<subscription_id>/', updateTransactionRecords, name='update-transactions'),
-    path('profile/', profile_view, name='profile'),
     path('cancel/', cancelSubscription, name='cancel')
 ]
